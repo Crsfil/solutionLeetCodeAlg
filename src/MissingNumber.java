1cb2806ecfcf5cfd4e05228fac9b5c9d610d6555
alextrizh <abespotifysh@gmail.com>
2025-01-31 00:15:34 +0300

    формула арифметической прогрессии

---
+++ src/MissingNumber.java
@@ -0,0 +1,20 @@
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] ints = InputUtils.readNumbers();
+        MissingNumber m = new MissingNumber();
+        System.out.println(m.missingNumber(ints));
+    }
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int S = n * (n + 1) / 2;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return S - sum;
+    }
+}
+
+
+// формула суммы арифметической прогрессии S = n*(n+1) / 2;
