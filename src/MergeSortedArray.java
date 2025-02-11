cc8adf24a8a9b69d26d9c2c18c5e0766c05a78ef
Crsfil <strizhovhotmail@gmail.com>
2025-02-11 23:47:37 +0300

    update 11/02 night

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -10,25 +10,24 @@ public class MergeSortedArray {
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int [] container;
-        int i = 0;
-        container = new int[m + n];
-        int cursor1 = 0, cursor2 = 0;
+        int i = m + n - 1;
+        int cursor1 = m - 1;
+        int cursor2 = n - 1;
 
-        while (cursor1 < m && cursor2 < n) {
-            if(nums1[cursor1] <= nums2[cursor2]){
-                container[i++] = nums1[cursor1++];
-            }else {
-                container[i++] = nums2[cursor2++];
+
+        while (cursor1 >= 0 && cursor2 >= 0) {
+            if (nums1[cursor1] > nums2[cursor2]) {
+                nums1[i--] = nums1[cursor1--];
+            } else {
+                nums1[i--] = nums2[cursor2--];
             }
         }
-        while (cursor1 < m) {
-            container[i++] = nums1[cursor1++];
+
+
+        while (cursor2 >= 0) {
+            nums1[i--] = nums2[cursor2--];
         }
 
-        while (cursor2 < n) {
-            container[i++] = nums2[cursor2++];
-        }
-
+        System.out.println(Arrays.toString(nums1));
     }
 }
