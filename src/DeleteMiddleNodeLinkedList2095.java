88b7e53d0b6780b58a4203d61a0f04dd8d2555b6
Crsfil <strizhovhotmail@gmail.com>
2025-03-18 20:44:03 +0300

    update 18/03 dinner

--- src/DeleteMiddleNodeLinkedList2095.java
+++ src/DeleteMiddleNodeLinkedList2095.java
@@ -1,3 +1,32 @@
 public class DeleteMiddleNodeLinkedList2095 {
+    public class ListNode {
+        int val;
+        ListNode next;
 
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode deleteMiddle(ListNode head) {
+        if(head.next == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
 }
---
+++ src/RemoveNthNodeFromEndList19.java
@@ -0,0 +1,18 @@
+public class RemoveNthNodeFromEndList19 {
+    public class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new List Node(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        f
+    }
+}
